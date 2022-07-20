c68c5f577b1b7048ee1bb61139e2b82a60c20715
unknown <ayadalshaikhli@gmail.com>
2022-07-19 22:33:24 -0700

    good tell cart

--- components/main/Nav.js
+++ components/main/Nav.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useContext} from "react";
 import Link from "next/link";
+import { CartContext } from "../../context/shopContext";
+import MiniCart from "./MiniCart";
 
 export default function Nav() {
+  const { cart, cartOpen, setCartOpen } = useContext(CartContext);
+
+  let cartQuantity = 0;
+  cart.map(item => {
+    return (cartQuantity += item?.variantQuantity);
+  })
   return (
     <header className="border-b-2 border-black sticky top-0 z-20  bg-white">
       <div className="flex items-center justify-between max-w-6xl pt-4 pb-2 px-4 mx-auto lg:max-w-screen-xl">
@@ -23,9 +31,10 @@ export default function Nav() {
           <a className="text-md font-bold cursor-pointer whitespace-nowrap">
             Sircles SWAG
           </a>
-          <a className="text-md font-bold cursor-pointer whitespace-nowrap">
-            Sircles SWAG
+          <a className="text-md font-bold cursor-pointer" onClick={() => setCartOpen(!cartOpen)}>
+          Cart ({cartQuantity})
           </a>
+          <MiniCart cart={cart}/>
         </div>
       </div>
     </header>
